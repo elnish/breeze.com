1553d59b39260487583b1a36864fcf10620485cb
elnish <a.shobotova@hotmail.com>
2019-06-27 10:00:57 +0300

    logo as btn, fixed active btn js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,21 @@
-//change color active btn
+// change color active btn
 let changeLang = document.querySelectorAll('.header__change-list')[0];
-let btns = changeLang.getElementsByClassName('change-list__item');
+let btns = changeLang.getElementsByClassName('change-list__link');
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', function() {
-  let current = document.getElementsByClassName('change-list__item_active');
-  current[0].className = current[0].className.replace(' change-list__item_active', '');
-  this.className += ' change-list__item_active';
+  let current = changeLang.getElementsByClassName('change-list__link_active');
+  current[0].className = current[0].className.replace(' change-list__link_active', '');
+  this.className += ' change-list__link_active';
   });
 }
 
 var changeCarrency = document.querySelectorAll('.header__change-list')[1];
-var btns2 = changeCarrency.getElementsByClassName('change-list__item_thin');
+var btns2 = changeCarrency.getElementsByClassName('change-list__link');
 for (let i = 0; i < btns2.length; i++) {
   btns2[i].addEventListener('click', function() {
-  var current = document.getElementsByClassName('change-list__item_active');
-  current[0].className = current[0].className.replace(' change-list__item_active', '');
-  this.className += ' change-list__item_active';
+  let current = changeCarrency.getElementsByClassName('change-list__link_active');
+  current[0].className = current[0].className.replace(' change-list__link_active', '');
+  this.className += ' change-list__link_active';
   });
 }
 
